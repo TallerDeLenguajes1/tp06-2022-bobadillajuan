1c9afa7f8fd207ee173a12650164cd6d9d85da77
bobadillajuan <juampi348@gmail.com>
2022-05-24 16:52:00 -0300

    Segundo punto Branch nuevo

--- Program.cs
+++ Program.cs
@@ -1,44 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Calculadora calc = new Calculadora(5);
-int numero = 0;
+Empleado empleado = new Empleado();
 
-Console.WriteLine("--- Calculadora. Valor inicial = 5---");
-Console.WriteLine("Selecciones la operacion: ");
-while (numero != 10)
-{
-numero = int.Parse(Console.ReadLine());
-switch (numero)
-{
-    case 1: 
-    calc.Suma(5);
-    Console.WriteLine(calc.Resultado);
-    break;
-
-    case 2:
-    calc.Resta(6);
-    Console.WriteLine(calc.Resultado);
-    break;
-
-    case 3:
-    calc.Multiplicar(2);
-    Console.WriteLine(calc.Resultado);
-    break;
-
-    case 4:
-    calc.Dividir(3);
-    Console.WriteLine(calc.Resultado);
-    break;
-
-    case 5:
-    calc.Limpiar();
-    Console.WriteLine(calc.Resultado);
-    break;
-
-    default:
-    Console.WriteLine("Fin de la calculadora. Resultado final: ");
-    Console.WriteLine(calc.Resultado);
-    numero = 10;
-    break;
-}
-}
+DateTime prueba = new DateTime(2000, 5, 5);
+empleado.Antiguedad(prueba);
