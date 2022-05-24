313a0aed3df8cd03edaf04da20095ab3a37f35e1
bobadillajuan <juampi348@gmail.com>
2022-05-24 15:15:13 -0300

    Primer commit

---
+++ calculadora.cs
@@ -0,0 +1,14 @@
+
+public class Calculadora{
+
+    public double Resultado;
+
+    public Calculadora(double valorInicial){
+        Resultado = valorInicial;
+    }
+
+    public void Suma(double valor){
+        Resultado += valor;
+    }
+
+}
