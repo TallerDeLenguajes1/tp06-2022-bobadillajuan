6f7b43276ba1420b070d76f6b26dd1e716596e1c
bobadillajuan <juampi348@gmail.com>
2022-06-05 21:28:04 -0300

    Ejercicio 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Empleado empleado = new Empleado();
-
-DateTime prueba = new DateTime(2000, 5, 5);
-empleado.Antiguedad(prueba);
+Console.WriteLine("Hello, World!");
